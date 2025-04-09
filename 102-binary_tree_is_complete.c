89b213bc8a6d35579d9e99172e111307b2f2890a
d-alshehri <10674@holbertonstudents.com>
2025-04-09 16:17:11 +0000

    Added function to check if a binary tree is complete + updated header file

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -9,15 +9,17 @@
  */
 queue_node_t *enqueue(queue_node_t **rear, const binary_tree_t *node)
 {
-    queue_node_t *new_node = malloc(sizeof(queue_node_t));
-    if (!new_node)
-        return (NULL);
-    new_node->node = node;
-    new_node->next = NULL;
-    if (*rear)
-        (*rear)->next = new_node;
-    *rear = new_node;
-    return (new_node);
+queue_node_t *new_node = malloc(sizeof(queue_node_t));
+if (!new_node)
+return (NULL);
+new_node->node = node;
+new_node->next = NULL;
+
+if (*rear)
+(*rear)->next = new_node;
+*rear = new_node;
+
+return new_node;
 }
 
 /**
@@ -27,16 +29,16 @@ queue_node_t *enqueue(queue_node_t **rear, const binary_tree_t *node)
  */
 const binary_tree_t *dequeue(queue_node_t **front)
 {
-    const binary_tree_t *node;
-    queue_node_t *tmp;
+const binary_tree_t *node;
+queue_node_t *tmp;
 
-    if (!*front)
-        return (NULL);
-    tmp = *front;
-    node = tmp->node;
-    *front = tmp->next;
-    free(tmp);
-    return (node);
+if (!*front)
+return (NULL);
+tmp = *front;
+node = tmp->node;
+*front = tmp->next;
+free(tmp);
+return (node);
 }
 
 /**
@@ -46,35 +48,32 @@ const binary_tree_t *dequeue(queue_node_t **front)
  */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-    queue_node_t *front = NULL, *rear = NULL;
-    const binary_tree_t *current;
-    int found_null = 0;
+queue_node_t *front = NULL, *rear = NULL;
+const binary_tree_t *current;
+int found_null = 0;
 
-    if (!tree)
-        return (0);
+if (!tree)
+return (0);
 
-    enqueue(&rear, tree);
-    front = rear;
+front = enqueue(&rear, tree);
 
-    while (front)
-    {
-        current = dequeue(&front);
+while (front)
+{
+current = dequeue(&front);
 
-        if (current == NULL)
-        {
-            found_null = 1;
-        }
-        else
-        {
-            if (found_null)
-            {
-
-                return (0);
-            }
-            enqueue(&rear, current->left);
-            enqueue(&rear, current->right);
-        }
-    }
-
-    return (1);
+if (current == NULL)
+{
+found_null = 1;
+}
+else
+{
+if (found_null)
+return (0);
+enqueue(&rear, current->left);
+enqueue(&rear, current->right);
+}
+}
+while (front)
+dequeue(&front);
+return (1);
 }
