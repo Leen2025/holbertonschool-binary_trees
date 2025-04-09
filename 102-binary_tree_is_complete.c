3b6c019a4021fe46d9675d8739e20187d0647e0a
d-alshehri <10674@holbertonstudents.com>
2025-04-09 15:27:14 +0000

    Added queue function prototypes and updated header file for binary_tree_is_complete implementation

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -1,39 +1,51 @@
 #include "binary_trees.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
-#include <stddef.h>
+#include <stdbool.h>  // Include for the bool type
 
+/**
+ * binary_tree_is_complete - Checks if a binary tree is complete
+ * @tree: Pointer to the root node of the tree to check
+ *
+ * Return: 1 if the tree is complete, 0 otherwise
+ */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
+queue_t *queue;
+bool is_leaf = false;
+
 if (!tree)
 return (0);
 
-queue_t *queue = create_queue();
+queue = create_queue();
 if (!queue)
 return (0);
 
 enqueue(queue, (binary_tree_t *)tree);
-bool is_leaf = false;
 
 while (!is_queue_empty(queue))
 {
 binary_tree_t *node = dequeue(queue);
 
 if (node->left)
+{
 enqueue(queue, node->left);
+}
 else
+{
 is_leaf = true;
+}
 
 if (node->right)
-enqueue(queue, node->right);
-else
-is_leaf = true;
-
-if (is_leaf && (node->left || node->right))
+{
+if (is_leaf)
 {
 free_queue(queue);
 return (0);
+}
+enqueue(queue, node->right);
+}
+else
+{
+is_leaf = true;
 }
 }
 
